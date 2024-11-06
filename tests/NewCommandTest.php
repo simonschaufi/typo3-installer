bfa5ea81ed7b1d0254e2528367d79105ca40b82a
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2024-11-07 00:58:48 +0100

    [TASK] Deactivate Windows as it fails with exit code 2

--- tests/NewCommandTest.php
+++ tests/NewCommandTest.php
@@ -6,6 +6,7 @@ namespace TYPO3\Installer\Console\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Tester\CommandTester;
 use TYPO3\Installer\Console\NewCommand;
 
@@ -32,7 +33,7 @@ class NewCommandTest extends TestCase
 
         $tester = new CommandTester($app->find('new'));
 
-        $statusCode = $tester->execute(['name' => $scaffoldDirectoryName]);
+        $statusCode = $tester->execute(['name' => $scaffoldDirectoryName], ['interactive' => false, 'verbosity' => OutputInterface::VERBOSITY_DEBUG]);
 
         self::assertSame(0, $statusCode);
         self::assertDirectoryExists($scaffoldDirectory . '/vendor');
